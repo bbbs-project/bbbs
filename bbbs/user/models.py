71572da74799324aa83188a90ec4f035af409073
Роман Рогачев <rogachew.r@yandex.ru>
2021-05-25 21:19:46 +0300

    Delete UserManager

--- bbbs/user/models.py
+++ bbbs/user/models.py
@@ -1,35 +1,7 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from bbbs.common.models import City
-
-class CustomAccountManager(BaseUserManager):
-
-    def create_user(self, username, email, password=None):
-        if username is None:
-            raise TypeError('Users should have a username')
-        if email is None:
-            raise TypeError('Users should have a Email')
-
-        user = self.model(username=username, email=self.normalize_email(email))
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, username, email, password=None):
-        if password is None:
-            raise TypeError('Password should not be none')
-
-        user = self.create_user(username, email, password)
-        user.is_superuser = True
-        user.is_verified = True
-        user.is_staff = True
-        user.role = user.ADMIN
-        user.save()
-        return user
-
-    def get_by_natural_key(self, email):
-        return self.get(email=email)
 
 
 class UserRole(models.TextChoices):
@@ -45,9 +17,6 @@ class CustomUser(AbstractUser):
         choices=UserRole.choices,
         default=UserRole.MENTOR,
     )
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
     
     class Meta:
         ordering = ['email']
